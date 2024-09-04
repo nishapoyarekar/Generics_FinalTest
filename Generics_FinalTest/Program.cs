da075c7e72d29bf32d326b8fe4d85bdbe8aa9418
Nisha Poyarekar <nisha.s.menon@gmail.com>
2024-09-03 21:05:33 -0400

    Add project files.

---
+++ Generics_FinalTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    static void Main()
+    {
+        // Create instance of SomeTypeCollection with int
+        SomeTypeCollection<int> collection = new SomeTypeCollection<int>();
+
+        // Add items
+        collection.AddItem(55);
+        collection.AddItem(10);
+        collection.AddItem(10);
+
+        // Retrieve an item by index
+        Console.WriteLine("Item at index 1: " + collection.GetItem(1)); // Output: 10
+
+        // Get sorted items in descending order
+        List<int> sortedItems = collection.GetSortedItemsDescending();
+        Console.WriteLine("Sorted items (descending): " + string.Join(", ", sortedItems)); // Output: 55,10,10
+    }
+}
---
+++ Generics_FinalTest/SomeTypeCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SomeTypeCollection<T> where T : struct, IComparable<T>
+{
+    // Private generic collection
+    private List<T> _items = new List<T>();
+
+    // Method to add items to the private collection
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    // Method that returns an item from the list by index
+    public T GetItem(int index)
+    {
+        return _items[index];
+    }
+
+    // Method that returns a sorted collection in descending order
+    public List<T> GetSortedItemsDescending()
+    {
+        return _items.OrderByDescending(item => item).ToList();
+    }
+}
